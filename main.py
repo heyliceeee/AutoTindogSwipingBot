69c118bd0edbe1ef5b81d3e46e63e0648808f516
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-16 11:10:52 +0100

    ✨feat: login

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.common import TimeoutException, NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+load_dotenv()
+
+TINDOG_URL = os.getenv("TINDOG_URL") # URL of the Tindog website
+
+chrome_options = webdriver.ChromeOptions() # Create a new option object
+chrome_options.add_experimental_option("detach", True) # Attach the driver to the background
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile") # Set the user data directory
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}") # Add the user data directory argument
+
+driver = webdriver.Chrome(options=chrome_options) # Instantiate the driver
+driver.get(TINDOG_URL) # Navigate to the GYM_URL
+wait = WebDriverWait(driver, 5) # Create a new wait object
+
+def login_automatically():
+    """
+    Login to the gym automatically
+    """
+    login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-tindog-login"))) # Wait for the login button to appear
+    login_btn.click() # Click the login button
+
+    login_facebark_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-facebark"))) # Wait for the login button to appear
+    login_facebark_btn.click() # Click the login button
+
+    base_window = driver.window_handles[0] # Get the current window handle
+    facebark_window = driver.window_handles[1] # Get the second window handle
+    driver.switch_to.window(facebark_window) # Switch to the second window
+
+    email_input = wait.until(EC.presence_of_element_located((By.ID, "email"))) # Wait for the email input field to appear
+    email_input.clear() # Clear the email input field
+    email_input.send_keys(os.getenv("TINDOG_EMAIL")) # Enter the email address from the .env file
+
+    password_input = wait.until(EC.presence_of_element_located((By.ID, "pass"))) # Wait for the password input field to appear
+    password_input.clear() # Clear the password input field
+    password_input.send_keys(os.getenv("TINDOG_PASSWORD")) # Enter the password from the .env file
+
+    submit_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit' and text()='Log In']"))) # Wait for the Submit button to appear
+    submit_btn.click() # Click the Submit button
+
+    driver.switch_to.window(base_window) # Switch back to the first window
+
+    # dismiss all requests
+    allow_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-primary"))) # Wait for the "Allow" button to appear
+    allow_btn.click() # Click the "Allow" button
+
+    not_interested_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-secondary"))) # Wait for the "Not interested" button to appear
+    not_interested_btn.click()  # Click the "Not interested" button
+
+    cookies_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-primary"))) # Wait for the "I accept" button to appear
+    cookies_btn.click() # Click the "I accept" button
+
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "main.tindog-swipe-container"))) # Wait for the main element to appear
+
+    return "OK"
+def retry(func, retries=7, description=None):
+    """
+    Retry a function if it fails
+    :param func: a function to retry
+    :param retries: retry count
+    :param description: description of the function
+    :return: function result
+    """
+    for attempt in range(1, retries + 1): # Retry up to retries times
+        try: # Try to execute the function
+            print(f"Attempt {attempt}/{retries} → {description}") # Print the attempt number
+            return func() # Return the function result
+
+        except (TimeoutException, NoSuchElementException, ElementClickInterceptedException, Exception) as e:
+            print(f"Failed attempt {attempt}: {e}") # Print the failure message
+            time.sleep(1) # Wait for 1 second before retrying
+
+    raise Exception(f"❌ All retries failed for: {description}") # If all retries fail, raise an exception
+
+retry(login_automatically, description="Login") # Call the login_automatically function with retries
